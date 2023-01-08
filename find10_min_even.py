2d4a18fb9138dcd6c7aeefe01e96f7ad72c69a04
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-08 21:51:52 +0500

    find min even

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,9 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
-
+    i, _m_even = 0, 0
+    while i < data[i]:
+        if _m_even > data[i] and data[i] % 2 == 0:
+            _m_even = data[i]
+        i += 1
+    return _m_even
