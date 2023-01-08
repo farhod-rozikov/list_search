dcf9d1f2ce1e76741a2ad255c9882952eb04efc7
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-08 22:12:51 +0500

    find max even

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,9 +5,9 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    i, _m_even = 0, 0
+    data.sort()
+    i = 0
     while i < len(data):
-        if _m_even < data[i] and data[i] % 2 == 0:
-            _m_even = data[i]
+        if data[i] % 2 == 0:
+            return data[i]            
         i += 1
-    return _m_even
